876ff3a2fa7f458ca81509b72a8d20020847bde6
Keanu <Gotrings@users.noreply.github.com>
2025-04-13 21:48:14 +0700

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(async ({ mode }: ConfigEnv): Promise<UserConfig> => 
       host: "::",
       port: 8080,
     },
+    base: '/HIMTECH/',
     plugins,
     resolve: {
       alias: {
